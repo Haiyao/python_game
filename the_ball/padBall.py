9ffa8fdf370b6d785b627f2fd8fee05448835c73
Nercececece <mma4017@icloud.com>
2016-03-24 11:35:03 +0800

    now i finish the ball

--- the_ball/padBall.py
+++ the_ball/padBall.py
@@ -9,16 +9,18 @@ tk = Tk()
 tk.title("Game-ball")
 tk.resizable(0,0)
 tk.wm_attributes("-topmost",1)
-canvas = Canvas(tk,width=400,height=500,bd=0,highlightthickness=0)
+canvas = Canvas(tk,width=500,height=400,bd=0,highlightthickness=0)
 canvas.pack()
 tk.update()
 
 #创建一个Ball类
 class Ball:
-    def __init__(self, canvas, color):
+    def __init__(self, canvas, paddle, color):
         self.canvas = canvas
+        self.paddle = paddle
         self.id = canvas.create_oval(10,10,25,25,fill=color)
-        
+        self.canvas.move(self.id, 245,100)
+        #将球移动到（245,100）处
         start = [-1,-2,-3,3,2,1]
         random.shuffle(start)
         self.x = start[0]
@@ -27,6 +29,7 @@ class Ball:
         #winfo_height获取当前画布的高度
         self.canvas_width = self.canvas.winfo_width()
         #winfo_width获取当前画布的宽度值
+        self.hit_bottom = False
     
     def draw(self):
         self.canvas.move(self.id, self.x, self.y)
@@ -39,22 +42,49 @@ class Ball:
         if pos[2] >=self.canvas_width:
             self.x = -3
         if pos[3] >= self.canvas_height:
+            self.hit_bottom = True
+        if  self.hit_paddle(pos) == True:
             self.y = -3
+            
+    def hit_paddle(self,pos):
+        paddle_pos = self.canvas.coords(self.paddle.id)
+        if pos[2] >= paddle_pos[0] and pos[0] <= paddle_pos[2]:
+            if pos[3] >= paddle_pos[1] and pos[3] <= paddle_pos[3]:
+                return True
+        return False
         
 class Paddle:
     def __init__(self,canvas,color):
         self.canvas = canvas
-        self.id = canvas.create_rectangle(100,300,150,310,fill=color)
-    
+        self.id = canvas.create_rectangle(0, 0, 100, 10,fill=color)
+        self.canvas.move(self.id, 200,300)
+        self.x = 0
+        self.canvas_weidth = self.canvas.winfo_width()
+        self.canvas.bind_all('<KeyPress-Left>', self.turn_left)
+        self.canvas.bind_all('<KeyPress-Right>', self.turn_right)
+        
+    def turn_left(self, evt):
+        self.x = -2
+        
+    def turn_right(self, evt):
+        self.x = 2
+        
     def draw(self):
-        pass
+        self.canvas.move(self.id, self.x, 0)
+        pos = self.canvas.coords(self.id)
+        if pos[0] <= 0:
+            self.x = 0
+        if pos[2] >= self.canvas_weidth:
+            self.x = 0
 
-ball = Ball(canvas, 'red')
 paddle = Paddle(canvas, 'blue')
+ball = Ball(canvas,paddle,'red')
 
 while 1:
-    paddle.draw()
-    ball.draw()
+    if ball.hit_bottom == False:
+        paddle.draw()
+        ball.draw()
     tk.update_idletasks()
     tk.update()
     time.sleep(0.01)
+    
